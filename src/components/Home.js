662ea562250237719f9f5a101fcc0274e354258f
Talha Zafar <talhabhai8@gmail.com>
2022-04-05 22:44:57 +0500

    Added home image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import Placeholder from 'react-bootstrap/Placeholder'
+import image from '../images/home.jpg'
 
 export default function Home() {
   return (
-      <>
-        <Placeholder xs={12} />      
-        <Placeholder xs={12} bg="primary" />
-        <Placeholder xs={12} bg="secondary" />
-        <Placeholder xs={12} bg="success" />
-        <Placeholder xs={12} bg="danger" />
-        <Placeholder xs={12} bg="warning" />
-        <Placeholder xs={12} bg="info" />
-        <Placeholder xs={12} bg="dark" />
-      </>
+    <img src={image}/>
   )
 }
